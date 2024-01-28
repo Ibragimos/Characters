dd42f84b66e77f9a759afc7100a3be5747caeecd
Marina <korhovama@mail.ru>
2024-01-28 13:40:46 +0300

    Added the implementation of the abstract Step method in the inheritors - Infantry

--- Main.cs
+++ Main.cs
@@ -14,6 +14,7 @@ class MainClass
         {
             return "rightTeam";
         }
+        
         return null;
     }
 
@@ -28,6 +29,7 @@ class MainClass
         {
             team = leftTeam;
         }
+
         return null;
     }
 
